7d60e29b448a13591744bc114ee0c09351b819f5
Rowen <rowen.simpson@wide-blue.com>
2019-08-13 13:18:50 +0100

    Exercise 3 stack commit

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            bool _stackActive = true;
+
+            Console.WriteLine("Stack GUI");
+            Console.WriteLine("1. Push");
+            Console.WriteLine("2. Push null");
+            Console.WriteLine("3. Pop");
+            Console.WriteLine("4. clear");
+            Console.WriteLine("5. Exit");
+
+            while (_stackActive)
+            {
+                Console.Write("------------Enter Number------------: ");
+                int caseSwitch = Int32.Parse(Console.ReadLine());
+                switch (caseSwitch)
+                {
+                    case 1:
+                        Console.Write("Input data to stack from keyboard: ");
+                        object pushInput = Console.ReadLine();
+                        try
+                        {
+                            stack.Push(pushInput);
+                        }
+                        catch (InvalidOperationException ex)
+                        {
+                            Console.WriteLine(ex);
+                        }
+                        break;
+                    case 2:
+                        try
+                        {
+                            stack.Push(null);
+                        }
+                        catch (InvalidOperationException ex)
+                        {
+                            Console.WriteLine(ex);
+                        }
+                        break;
+                    case 3:
+                        try
+                        {
+                            Console.WriteLine(stack.Pop());
+                        }
+                        catch (InvalidOperationException ex)
+                        {
+                            Console.WriteLine(ex);
+                        }
+                        break;
+                    case 4:
+                        stack.Clear();
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Default case");
+                        break;
+                }
+            }
+        }
+    }
+}
+
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace Stack
+{
+    class Stack
+    {
+        ArrayList stack = null;
+
+        public Stack()
+        {
+            stack = new ArrayList();
+        }
+
+        public void Push(object obj)
+        {
+            if (obj != null)
+            {
+                stack.Add(obj);
+            }
+            else
+                throw new InvalidOperationException("obj");
+        }
+
+        public object Pop()
+        {
+            object result;
+            int lastElement = stack.Count;
+
+            if (lastElement != 0)
+            {
+                result = stack[lastElement - 1];
+                stack.RemoveAt(lastElement - 1);
+            }
+                
+            else
+                throw new InvalidOperationException("lastElement");
+            return result;
+        }
+
+        public void Clear()
+        {
+            stack.Clear();
+        }
+    }
+}
